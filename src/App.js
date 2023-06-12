b4aedb2f0fe9389d935868cb9fc3274c4e4f8cd9
Imran <imranshaha41@gmail.com>
2023-06-12 13:47:42 +0530

    Landing page left part is done with in MainComponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './components/Common/Footer';
 import Header from './components/Common/Header';
+import MainComponent from './components/LandingPage/MainComponent';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <MainComponent/>
       {/* <Footer/> */}
     </div>
   );
