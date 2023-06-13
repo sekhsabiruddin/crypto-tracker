f647256637fc0b530930a695b69e19f3bedd1181
Imran <imranshaha41@gmail.com>
2023-06-13 19:24:07 +0530

    Tabs component is added in dashbord tab from mui

--- src/components/Dashboard/Tabs/index.js
+++ src/components/Dashboard/Tabs/index.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { ThemeProvider, createTheme } from '@mui/material';
 
-function TabsComponent() {
+export default function TabsComponent() {
+  const [value, setValue] = useState('grid');
+
+  const style = {
+    color : 'var(--white)',
+    width : '50vw',
+    fontSize : '1.2rem',
+    fontWeight : 600,
+    fontFamily : 'Inter',
+    textTransform : 'capitalize',
+  }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const theme = createTheme({
+    palette : {
+        primary : {
+            main : '#3a80e9'
+        }
+    }
+  })
+
   return (
-    <div>TabsComponent</div>
-  )
+    <ThemeProvider theme={theme}>
+      <TabContext value={value}>
+          <TabList onChange={handleChange} variant='fullWidth'>
+            <Tab label="Grid" value="grid" sx={style}/>
+            <Tab label="List" value="list" sx={style}/>
+          </TabList>
+        <TabPanel value="grid">
+            <div>
+                mapping for grid
+            </div>
+        </TabPanel>
+        <TabPanel value="list">
+            <div>
+                mapping for list
+            </div>
+        </TabPanel>
+      </TabContext>
+    </ThemeProvider>
+  );
 }
-
-export default TabsComponent
