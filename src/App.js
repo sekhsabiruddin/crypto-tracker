a4ef4cb328ada2a0e490b85e8e73bcd816ff1e72
Imran <imranshaha41@gmail.com>
2023-06-13 17:53:55 +0530

    react router dom libarary added and routing part is done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Footer from './components/Common/Footer';
-import Header from './components/Common/Header';
-import MainComponent from './components/LandingPage/MainComponent';
+import HomePage from './pages/Home';
+import DashboardPage from './pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <MainComponent/>
-      {/* <Footer/> */}
+          <BrowserRouter>
+                <Routes>
+                      <Route path='/' element={<HomePage/>} />
+                      <Route path='/dashboard' element={<DashboardPage/>}/>
+                </Routes>
+          </BrowserRouter>
     </div>
   );
 }
--- src/components/Common/Header/drawer.js
+++ src/components/Common/Header/drawer.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Drawer from '@mui/material/Drawer';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import { IconButton } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 
 export default function TemporaryDrawer() {
@@ -19,18 +20,18 @@ export default function TemporaryDrawer() {
         onClose={()=>setOpen(false)}
         >
         <div className='drawer-div'>
-            <a href='/'>
+            <Link to='/'>
                 <p className='link'>Home</p>
-            </a>
-            <a href='/'>
+            </Link>
+            <Link to='/compare'>
                 <p className='link'>Compare</p>
-            </a>
-            <a href='/'>
+            </Link>
+            <Link to='/watchlist'>
                 <p className='link'>Watchlist</p>
-            </a>
-            <a href='/'>
+            </Link>
+            <Link to='/dashboard'>
                 <p className='link'>Dashboard</p>
-            </a>
+            </Link>
         </div>
         </Drawer>
 
