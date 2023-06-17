e11342f02c7aff400894d4ccbb93f3636766cb6b
Imran <imranshaha41@gmail.com>
2023-06-17 18:34:24 +0530

    using chart.js and react-chartjs-2 library build the chart component and used it

---
+++ src/functions/convertDate.js
@@ -0,0 +1,5 @@
+export const convertDate = (number) =>{
+    let myDate = new Date(number);
+
+    return myDate.getDate() + "/" + (myDate.getMonth() + 1) ;
+}
--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -7,12 +7,15 @@ import List from '../components/Dashboard/List';
 import CoinInfo from '../components/Coin/CoinInfo';
 import { getCoinData } from '../functions/getCoinData';
 import { getCoinPrices } from '../functions/getCoinPrices';
+import LineChart from '../components/Coin/LineChart';
+import { convertDate } from '../functions/convertDate';
 
 function CoinPage() {
     const {id} = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [coinData, setCoinData] = useState();
-    const [days, setDays] = useState(7);
+    const [days, setDays] = useState(30);
+    const [chartData, setChartData] = useState({});
 
     useEffect(() => {
        if(id){
@@ -28,7 +31,21 @@ function CoinPage() {
             coinObject(setCoinData,data);
             const prices = await getCoinPrices(id,days);
             if(prices.length > 0){
-                console.log("prices get")
+                console.log("prices get");
+                setChartData({
+                    labels : prices.map((price)=> convertDate(price[0])),
+                    datasets : [
+                        {
+                            data : prices.map((price)=>price[1]),
+                            borderColor : "#3a80e9",
+                            borderWidth : 2,
+                            fill : true,
+                            tension : 0.25,
+                            backgroundColor : "rgba(58,128,233,0.1)",
+                            pointRadius : 0
+                        }
+                    ]
+                })
                 setIsLoading(false);
             }
         }
@@ -43,6 +60,9 @@ function CoinPage() {
                 (  <React.Fragment>
                         <div className='grey-wrapper'>
                              <List coin={coinData}/>
+                        </div>
+                        <div className='grey-wrapper'>
+                            <LineChart chartData={chartData}/>
                         </div>
                         <CoinInfo heading={coinData.name} desc={coinData.desc} />
                     </React.Fragment>
