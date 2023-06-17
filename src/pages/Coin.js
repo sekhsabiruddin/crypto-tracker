53b664441383fd0b5b2d753c75e73d02e09259eb
Imran <imranshaha41@gmail.com>
2023-06-17 13:53:45 +0530

    fetch prices and making separate function for each api call

--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -2,34 +2,38 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Header from '../components/Common/Header';
 import Loader from '../components/Common/Loader';
-import axios from 'axios';
 import { coinObject } from '../functions/coinObject';
 import List from '../components/Dashboard/List';
 import CoinInfo from '../components/Coin/CoinInfo';
+import { getCoinData } from '../functions/getCoinData';
+import { getCoinPrices } from '../functions/getCoinPrices';
 
 function CoinPage() {
     const {id} = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [coinData, setCoinData] = useState();
+    const [days, setDays] = useState(7);
 
     useEffect(() => {
        if(id){
-        axios.get(
-            `https://api.coingecko.com/api/v3/coins/${id}`
-        )
-        .then((response)=>{
-            console.log("Response >>> ",response)
-            coinObject(setCoinData, response.data)
-            setIsLoading(false)
-        })
-        .catch((error)=>{
-            console.log("error >>> ",error)
-            setIsLoading(false)
-        })
+            getData()
        }
+       
     }, [id]);
 
-    console.log("coinData >> ",coinData)
+    async function getData(){
+
+        const data = await getCoinData(id);
+        if(data){
+            coinObject(setCoinData,data);
+            const prices = await getCoinPrices(id,days);
+            if(prices.length > 0){
+                console.log("prices get")
+                setIsLoading(false);
+            }
+        }
+    }
+
   return (
     <>
         <div>
