bed4b3ce6b6686d56a67720b906708b8bd28995a
Imran <imranshaha41@gmail.com>
2023-06-20 17:37:10 +0530

    mouse up,down and mouse movement with cursor has done

--- src/App.js
+++ src/App.js
@@ -5,10 +5,48 @@ import DashboardPage from './pages/Dashboard';
 import CoinPage from './pages/Coin';
 import ComparePage from './pages/ComparePage';
 import WatchlistPage from './pages/WatchList';
+import { useEffect } from 'react';
 
 function App() {
+  let cursor;
+  let cursorPointer;
+
+  useEffect(() => {
+    cursor = document.getElementById("cursor");
+    cursorPointer = document.getElementById("cursor-pointer");
+
+    document.body.addEventListener("mousemove", function (e) {
+      return (
+        (cursor.style.left = e.clientX + "px"),
+        (cursor.style.top = e.clientY + "px"),
+        (cursorPointer.style.left = e.clientX + "px"),
+        (cursorPointer.style.top = e.clientY + "px")
+      );
+    });
+
+    document.body.addEventListener("mousedown", function (e) {
+      return (
+        (cursor.style.height = "0.5rem"),
+        (cursor.style.width = "0.5rem"),
+        (cursorPointer.style.height = "3rem"),
+        (cursorPointer.style.width = "3rem")
+      );
+    });
+
+    document.body.addEventListener("mouseup", function (e) {
+      return (
+        (cursor.style.height = "0.3rem"),
+        (cursor.style.width = "0.3rem"),
+        (cursorPointer.style.height = "2rem"),
+        (cursorPointer.style.width = "2rem")
+      );
+    });
+  }, []);
+
   return (
     <div className="App">
+        <div className="cursor" id="cursor" />
+        <div className="cursor-pointer" id="cursor-pointer" />
           <BrowserRouter>
                 <Routes>
                       <Route path='/' element={<HomePage/>} />
